0c2534175b10d7043a8af15689434e62944ad171
yvan99 <ishimweyvan90@gmail.com>
2023-03-30 00:10:48 +0200

    db migr ft

--- app/Models/Farmer.php
+++ app/Models/Farmer.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Farmer extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable,HasFactory;
 
     protected $fillable = [
         'name', 'email', 'phone', 'address', 'password',
