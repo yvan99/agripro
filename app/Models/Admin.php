7136ad03b1d45242214d3994efbe77f6706cc793
yvan99 <ishimweyvan90@gmail.com>
2023-03-30 00:01:13 +0200

    db migr ft

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    use HasFactory;
+    use Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'phone', 'address', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
