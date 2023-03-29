52ff661f5b3cb95a484f15817333900f22650f46
yvan99 <ishimweyvan90@gmail.com>
2023-03-30 00:00:36 +0200

    db migr ft

--- database/factories/FarmerFactory.php
+++ database/factories/FarmerFactory.php
@@ -11,10 +11,15 @@ class FarmerFactory extends Factory
      *
      * @return array
      */
+    protected $model = Farmer::class;
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'password' => bcrypt('password'),
         ];
     }
 }
