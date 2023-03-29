cd51e1267c4122e794f0146009b26c958e5928e8
yvan99 <ishimweyvan90@gmail.com>
2023-03-30 00:01:41 +0200

    db migr ft

---
+++ app/Models/Farmer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Farmer extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'phone', 'address', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
