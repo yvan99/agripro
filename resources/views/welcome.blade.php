61f787e696eb026bb828e90bbbf19182fb23fd11
yvan99 <ishimweyvan90@gmail.com>
2023-03-30 22:52:54 +0200

    feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,115 +24,13 @@
             </div>
         </div>
     </div>
-    <!--====== Header Navigation ======-->
-    <div class="header-navigation">
-        <div class="nav-overlay"></div>
-        <div class="container-fluid">
-            <div class="primary-menu">
-                <!--====== Site Branding ======-->
-                <div class="site-branding">
-                    <a href="index.html" class="brand-logo"><img src="assets/images/logo/logo-black.png"
-                            alt="Site Logo"></a>
-                </div>
-                <!--====== Nav Menu ======-->
-                <div class="nav-menu">
-                    <!--====== Site Branding ======-->
-                    <div class="mobile-logo mb-30 d-block d-xl-none">
-                        <a href="index.html" class="brand-logo"><img src="assets/images/logo/logo-black.png"
-                                alt="Site Logo"></a>
-                    </div>
-                    <!--=== Nav Search ===-->
-                    <div class="nav-search mb-30 d-block d-xl-none ">
-                        <form>
-                            <div class="form_group">
-                                <input type="email" class="form_control" placeholder="Search Here" name="email"
-                                    required>
-                                <button class="search-btn"><i class="fas fa-search"></i></button>
-                            </div>
-                        </form>
-                    </div>
-                    <!--====== main Menu ======-->
-                    <nav class="main-menu">
-                        <ul>
-                            <li class="menu-item has-children"><a href="#">Home</a>
-                                <ul class="sub-menu">
-                                    <li><a href="index.html">Home 01</a></li>
-                                    <li><a href="index-2.html">Home 02</a></li>
-                                    <li><a href="index-3.html">Home 03</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item"><a href="about.html">About</a></li>
-                            <li class="menu-item has-children"><a href="#">Service</a>
-                                <ul class="sub-menu">
-                                    <li><a href="service.html">Our Service</a></li>
-                                    <li><a href="service-details.html">Service Details</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item has-children"><a href="#">Portfolio</a>
-                                <ul class="sub-menu">
-                                    <li><a href="project-grid.html">Project Grid</a></li>
-                                    <li><a href="project-grid-2-column.html">Project Grid Two</a></li>
-                                    <li><a href="project-masonry.html">Project Masonry</a></li>
-                                    <li><a href="project-details.html">Project Details</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item has-children"><a href="#">Blog</a>
-                                <ul class="sub-menu">
-                                    <li><a href="blog-standard.html">Blog Standard</a></li>
-                                    <li><a href="blog-details.html">Blog Details</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item has-children"><a href="#">Pages</a>
-                                <ul class="sub-menu">
-                                    <li><a href="team.html">Our Team</a></li>
-                                    <li><a href="shop.html">Our Shop</a></li>
-                                    <li><a href="product-details.html">Product Details</a></li>
-                                    <li><a href="testimonials.html">Testimonial</a></li>
-                                    <li><a href="pricing.html">Pricing</a></li>
-                                    <li><a href="faqs.html">Faq</a></li>
-                                    <li><a href="contact.html">Contact</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item">
-                                <div class="search-btn" data-toggle="modal" data-target="#search-modal"><i
-                                        class="far fa-search"></i></div>
-                            </li>
-                        </ul>
-                    </nav>
-                    <!--====== Menu Button ======-->
-                    <div class="menu-button mt-40 d-xl-none">
-                        <a href="contact.html" class="main-btn secondary-btn">Get a Quote</a>
-                    </div>
-                </div>
-                <!--====== Nav Right Item ======-->
-                <div class="nav-right-item">
-                    <div class="menu-button d-xl-block d-none">
-                        <a href="contact.html" class="main-btn primary-btn">Get a Quote</a>
-                    </div>
-                    <div class="bar-button" data-toggle="modal" data-target="#sidebar-modal">
-                        <img src="assets/images/bar.png" alt="Image">
-                    </div>
-                    <div class="navbar-toggler">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
 </header>
 <!--====== End Area ======-->
 <!--====== Start Banner Section ======-->
 <section class="banner-section">
     <!--=== Hero Wrapper ===-->
     <div class="hero-wrapper-one gray-bg">
-        <div class="shape shape-one animate-float-y"><span><img src="assets/images/hero/shape-1.png"
-                    alt="shape"></span></div>
-        <div class="shape shape-two animate-float-x"><span><img src="assets/images/hero/shape-2.png"
-                    alt="shape"></span></div>
-        <div class="shape shape-three animate-float-x"><span><img src="assets/images/hero/shape-3.png"
-                    alt="shape"></span></div>
         <div class="container">
             <div class="row align-items-lg-center">
                 <div class="col-xl-6 col-lg-12">
@@ -152,7 +50,7 @@
                 <div class="col-xl-6 col-lg-12">
                     <!--=== Hero Image Box ===-->
                     <div class="hero-image-box d-xl-block d-none wow fadeInRight" data-wow-delay=".75s">
-                        <img src="assets/images/hero/hero-three_one.jpg" alt="Hero Image">
+                        <img src="{{asset('homepage/images/hero/hero-three_one.jpg')}}" alt="Hero Image">
                         <div class="shape hero-svg">
                             <svg width="237" height="569" viewBox="0 0 237 569" fill="none">
                                 <path
@@ -280,5 +178,5 @@
     </div>
 </section>
 <!--====== End Features Section ======-->
-@include('components.footer')
+@include('components.homefooter')
 @include('components.homejs')
