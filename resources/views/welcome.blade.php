41736cfc6ce7231ac5fcd0aafa0c550a65778720
yvan99 <ishimweyvan90@gmail.com>
2023-03-30 22:39:37 +0200

    feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -995,33 +995,4 @@
                 </div>
             </div>
         </footer><!--====== End Footer ======-->
-        <!--====== Back To Top  ======-->
-        <a href="#" class="back-to-top" ><i class="far fa-angle-up"></i></a>
-        <!--====== Jquery js ======-->
-        <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/vendor/jquery-3.6.0.min.js"></script>
-        <!--====== Bootstrap js ======-->
-        <script src="assets/vendor/popper/popper.min.js"></script>
-        <!--====== Bootstrap js ======-->
-        <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-        <!--====== Slick js ======-->
-        <script src="assets/vendor/slick/slick.min.js"></script>
-        <!--====== Magnific js ======-->
-        <script src="assets/vendor/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
-        <!--====== Isotope js ======-->
-        <script src="assets/vendor/isotope.min.js"></script>
-        <!--====== Imagesloaded js ======-->
-        <script src="assets/vendor/imagesloaded.min.js"></script>
-        <!--====== Counterup js ======-->
-        <script src="assets/vendor/jquery.counterup.min.js"></script>
-        <!--====== Waypoints js ======-->
-        <script src="assets/vendor/jquery.waypoints.js"></script>
-        <!--====== Nice-select js ======-->
-        <script src="assets/vendor/nice-select/js/jquery.nice-select.min.js"></script>
-        <!--====== jquery UI js ======-->
-        <script src="assets/vendor/jquery-ui/jquery-ui.min.js"></script>
-        <!--====== WOW js ======-->
-        <script src="assets/vendor/wow.min.js"></script>
-        <!--====== Main js ======-->
-        <script src="assets/js/theme.js"></script>
-    </body>
-</html>
+@include('components.homejs')
