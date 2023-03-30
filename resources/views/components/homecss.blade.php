f3315c3933ecacdb87344d260660fc75fdecf195
yvan99 <ishimweyvan90@gmail.com>
2023-03-30 22:32:13 +0200

    feature

---
+++ resources/views/components/homecss.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <!--====== Required meta tags ======-->
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="description" content="Landscaping, Gardening, Florists, Groundskeeping">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!--====== Title ======-->
+        <title>{{env("APP_NAME")}}</title>
+        <!--====== Favicon Icon ======-->
+        <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/png">
+        <!--====== Google Fonts ======-->
+        <link href="../../../fonts.googleapis.com/css2e6da.css?family=Montserrat:wght@400;500;600;700&amp;family=Roboto:wght@400;500;700&amp;display=swap" rel="stylesheet">
+        <!--====== Flaticon css ======-->
+        <link rel="stylesheet" href="assets/fonts/flaticon/flaticon_gadden.css">
+        <!--====== FontAwesome css ======-->
+        <link rel="stylesheet" href="assets/fonts/fontawesome/css/all.min.css">
+        <!--====== Bootstrap css ======-->
+        <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+        <!--====== magnific-popup css ======-->
+        <link rel="stylesheet" href="assets/vendor/magnific-popup/dist/magnific-popup.css">
+        <!--====== Slick-popup css ======-->
+        <link rel="stylesheet" href="assets/vendor/slick/slick.css">
+        <!--====== Jquery UI css ======-->
+        <link rel="stylesheet" href="assets/vendor/jquery-ui/jquery-ui.min.css">
+        <!--====== Nice Select css ======-->
+        <link rel="stylesheet" href="assets/vendor/nice-select/css/nice-select.css">
+        <!--====== Animate css ======-->
+        <link rel="stylesheet" href="assets/vendor/animate.css">
+        <!--====== Default css ======-->
+        <link rel="stylesheet" href="assets/css/default.css">
+        <!--====== Style css ======-->
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <!--====== Start Preloader ======-->
+        <div class="preloader">
+            <div class="loader">
+                <div class="pre-shadow"></div>
+                <div class="pre-box"></div>
+            </div>
+        </div><!--====== End Preloader ======-->
