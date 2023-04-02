af63b9de3cc153971285a868d7ea43d7d7a372e9
yvan99 <ishimweyvan90@gmail.com>
2023-04-03 00:11:11 +0200

    change colors

--- resources/views/components/dashcss.blade.php
+++ resources/views/components/dashcss.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Library / Plugin Css Build -->
+    <link rel="stylesheet" href="{{ asset('dashboard/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('dashboard/css/core/libs.min.css') }}">
     <!-- qompac-ui Design System Css -->
     <link rel="stylesheet" href="{{ asset('dashboard/css/qompac-ui.minf700.css?v=1.0.1') }}">
--- resources/views/farmer/register.blade.php
+++ resources/views/farmer/register.blade.php
@@ -3,13 +3,14 @@
 <body class="">
     <div class="wrapper">
         <section class="login-content overflow-hidden">
-            <div class="row no-gutters align-items-center bg-primary">
+            <div class="row no-gutters align-items-center bg-primary auth-screen">
                 <div class="col-md-12 col-lg-12 align-self-center">
                     <div class="row justify-content-center">
                         <div class="col-md-6 mt-5 mb-5">
                             <div class="card auth-card  d-flex justify-content-center mb-0">
                                 <div class="card-header">
                                     <h3>{{ __('Register as a Farmer') }}</h3>
+                                    <p class="mt-4">Fill In your account details</p>
                                 </div>
 
                                 <div class="card-body">
@@ -114,7 +115,7 @@
 
                                         <div class="form-group row mb-0">
                                             <div class="col-md-6 offset-md-4">
-                                                <button type="submit" class="btn btn-primary">
+                                                <button type="submit" class="btn btn-warning">
                                                     {{ __('Register') }}
                                                 </button>
                                             </div>
