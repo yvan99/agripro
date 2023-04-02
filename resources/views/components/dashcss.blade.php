65d7a743c7ed4b14a04cc61ae6fa28cea76002e7
yvan99 <ishimweyvan90@gmail.com>
2023-04-02 20:36:47 +0200

    feature

---
+++ resources/views/components/dashcss.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Library / Plugin Css Build -->
+    <link rel="stylesheet" href="{{ asset('css/core/libs.min.css') }}">
+    <!-- qompac-ui Design System Css -->
+    <link rel="stylesheet" href="{{ asset('css/qompac-ui.minf700.css?v=1.0.1') }}">
+
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="{{ asset('css/custom.minf700.css?v=1.0.1') }}">
+    <!-- Dark Css -->
+    <link rel="stylesheet" href="{{ asset('css/dark.minf700.css?v=1.0.1') }}">
+
+    <!-- Customizer Css -->
+    <link rel="stylesheet" href="{{ asset('css/customizer.minf700.css?v=1.0.1') }}">
+
+    <!-- RTL Css -->
+    <link rel="stylesheet" href="{{ asset('css/rtl.minf700.css?v=1.0.1') }}">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+        rel="stylesheet">
+</head>
