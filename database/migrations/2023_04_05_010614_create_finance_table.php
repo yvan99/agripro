d5160631ea5e94669e7086d9a4992cc7e99a2e09
yvan99 <ishimweyvan90@gmail.com>
2023-04-05 03:07:03 +0200

    feature

--- database/migrations/2023_04_05_010614_create_finance_table.php
+++ database/migrations/2023_04_05_010614_create_finance_table.php
@@ -15,6 +15,16 @@ class CreateFinanceTable extends Migration
     {
         Schema::create('finance', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('farmer_id')->constrained('farmers')->onDelete('cascade');
+            $table->foreignId('season_id')->constrained('seasons')->onDelete('cascade');
+            $table->decimal('production_cost', 10, 2);
+            $table->decimal('income', 10, 2);
+            $table->decimal('gross_margin', 10, 2);
+            $table->decimal('labor_cost', 10, 2);
+            $table->decimal('fertilizer_cost', 10, 2);
+            $table->decimal('pesticide_cost', 10, 2);
+            $table->decimal('irrigation_cost', 10, 2);
+            $table->decimal('net_profit', 10, 2);
             $table->timestamps();
         });
     }
