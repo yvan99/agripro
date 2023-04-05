d8a8225528ae4d5f234b2016b143d48206db7d5c
yvan99 <ishimweyvan90@gmail.com>
2023-04-05 02:54:06 +0200

    feature

--- app/Models/Water.php
+++ app/Models/Water.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Water extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'farmer_id',
+        'season_id',
+        'crop_id',
+        'amount',
+        'irrigation_type',
+        'irrigation_frequency',
+        'cost',
+    ];
+
+    public function farmer()
+    {
+        return $this->belongsTo(Farmer::class);
+    }
+
+    public function season()
+    {
+        return $this->belongsTo(Season::class);
+    }
+
+    public function crop()
+    {
+        return $this->belongsTo(Crop::class);
+    }
 }
