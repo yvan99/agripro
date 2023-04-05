61f33e2fa025869e1d0728d4c52b75fb0bb4c766
yvan99 <ishimweyvan90@gmail.com>
2023-04-05 02:56:07 +0200

    feature

---
+++ app/Models/Energy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Energy extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'farmer_id',
+        'season_id',
+        'crop_id',
+        'energy_type',
+        'cost',
+    ];
+
+    public function farmer()
+    {
+        return $this->belongsTo(Farmer::class);
+    }
+
+    public function season()
+    {
+        return $this->belongsTo(Season::class);
+    }
+
+    public function crop()
+    {
+        return $this->belongsTo(Crop::class);
+    }
+}
