468d63805adaaefbbba1c823c4209e41f38e6af9
yvan99 <ishimweyvan90@gmail.com>
2023-04-06 18:59:31 +0200

    feature

---
+++ app/Http/Controllers/WaterController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Season;
+use App\Models\Water;
+use Illuminate\Http\Request;
+
+class WaterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $waters = Water::all();
+        $seasons = Season::all();
+        return view('water.index', compact('waters', 'seasons'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'amount' => 'required|numeric',
+            'irrigation_type' => 'required|string',
+            'irrigation_frequency' => 'required|integer',
+            'cost' => 'required|numeric',
+            'season_id' => 'required|exists:seasons,id',
+            'crop_id' => 'required|exists:crops,id',
+        ]);
+        $water = new Water();
+        $water->amount = $validated['amount'];
+        $water->irrigation_type = $validated['irrigation_type'];
+        $water->irrigation_frequency = $validated['irrigation_frequency'];
+        $water->cost = $validated['cost'];
+        $water->timestamp = now();
+        $water->farmer_id = auth()->user()->farmer->id;
+        $water->season_id = $validated['season_id'];
+        $water->crop_id = $validated['crop_id'];
+        $water->save();
+        return redirect()->route('water.index')->with('success', 'Water added successfully');
+    }
+}
