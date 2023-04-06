02f59228bfefa421da231af449ac89d10fed90df
yvan99 <ishimweyvan90@gmail.com>
2023-04-06 23:56:39 +0200

    feature

--- app/Http/Controllers/WaterController.php
+++ app/Http/Controllers/WaterController.php
@@ -17,14 +17,14 @@ class WaterController extends Controller
     public function index()
     {
         $waters = Water::where('farmer_id', auth()->user()->id)->get();
-        $seasons = Season::where('farmer_id', auth()->user()->id)->get();
-        $crops = Crop::all();
+        $seasons = Season::all();
+        $crops = Crop::where('farmer_id', auth()->user()->id)->get();
         return view('water.index', compact('waters', 'seasons','crops'));
     }
     public function waterAdmin()
     {
-        $waters = Water::all();
-        $seasons = Season::all();
+        $waters = Water::where('farmer_id', auth()->user()->id)->get();
+        $seasons = Season::where('farmer_id', auth()->user()->id)->get();
         $crops = Crop::all();
         return view('water.index', compact('waters', 'seasons','crops'));
     }
