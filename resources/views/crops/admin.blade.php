efa627e81aa28b9a059a78d01adf53675ed98189
yvan99 <ishimweyvan90@gmail.com>
2023-04-08 21:20:35 +0200

    last commit

--- resources/views/crops/admin.blade.php
+++ resources/views/crops/admin.blade.php
@@ -41,7 +41,7 @@
                 <div class="card">
                     <div class="card-header d-flex justify-content-between">
                         <div class="header-title">
-                            <h4 class="card-title">Crops List</h4>
+                            <h4 class="card-title">Crops Report List</h4>
 
                         </div>
                     </div>
