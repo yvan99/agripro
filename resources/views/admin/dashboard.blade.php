de0a5db8c251976400e3643961da5ee69983b3f9
yvan99 <ishimweyvan90@gmail.com>
2023-04-08 20:09:40 +0200

    feature

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,7 +7,7 @@
         <!--Nav End-->
     </div>
     <div class="content-inner container-fluid pb-0" id="page_layout">
-        <div class="d-flex justify-content-between align-items-center flex-wrap mb-5 gap-3">
+        <div class="d-flex justify-content-between align-items-center flex-wrap mb-5 mt-5 gap-3">
             <div class="d-flex flex-column">
                 <h3>Hello , {{ Auth::user()->name }}</h3>
                 <p class="mb-0">Analytics Dashboard</p>
@@ -15,69 +15,222 @@
 
         </div>
         <div class="row">
-            <div class="col-lg-8 col-xl-8">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="card card-block card-stretch card-height">
-                            <div class="flex-wrap card-header d-flex justify-content-between align-items-center">
-                                <div class="header-title">
-                                    <h4>Sales Stastics</h4>
-                                </div>
-                                <div class="d-flex">
-                                    <div class="mx-3">
-                                        <p class="mb-0"><svg class="text-primary" width="10" height="10"
-                                                viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <circle cx="5" cy="5" r="5"
-                                                    fill="currentColor" />
-                                            </svg> Total Sales </p>
-                                    </div>
-                                    <div class="mx-3">
-                                        <p class="mb-0"><svg class="text-secondary" width="10" height="10"
-                                                viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <circle cx="5" cy="5" r="5"
-                                                    fill="currentColor" />
-                                            </svg> Total Expense</p>
-                                    </div>
-                                    <div class="">
-                                        <p class="mb-0"><svg class="text-tertiray" width="10" height="10"
-                                                viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <circle cx="5" cy="5" r="5"
-                                                    fill="currentColor" />
-                                            </svg> Total Profit</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <div id="sales-chart-02" class="sales-chart-02"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 col-sm-12">
-                <div class="card card-block card-stretch card-height">
-                    <div class="flex-wrap card-header d-flex justify-content-between align-items-center">
-                        <div class="header-title">
-                            <h4>Sales Anylsis</h4>
-                        </div>
-                        <div class="dropdown">
-                            <a href="#" class="text-gray dropdown-toggle" id="dropdownMenuButton29"
-                                data-bs-toggle="dropdown" aria-expanded="false">All Tasks</a>
-                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton29"
-                                style="">
-                                <li><a class="dropdown-item" href="#">This Week</a></li>
-                                <li><a class="dropdown-item" href="#">This Month</a></li>
-                                <li><a class="dropdown-item" href="#">This Year</a></li>
-                            </ul>
-                        </div>
-                    </div>
+            <div class="col-xl-4">
+                <div class="card">
                     <div class="card-body">
-                        <div id="sales-chart-04" class="sales-chart-04"></div>
+                        <div class="d-flex justify-content-between align-itmes-center">
+                            <div>
+                                <div class="p-3 rounded bg-soft-primary">
+                                    <svg class="icon-30" width="30" viewBox="0 0 24 24" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M11.997 15.1746C7.684 15.1746 4 15.8546 4 18.5746C4 21.2956 7.661 21.9996 11.997 21.9996C16.31 21.9996 19.994 21.3206 19.994 18.5996C19.994 15.8786 16.334 15.1746 11.997 15.1746Z"
+                                            fill="currentColor"></path>
+                                        <path opacity="0.4"
+                                            d="M11.9971 12.5838C14.9351 12.5838 17.2891 10.2288 17.2891 7.29176C17.2891 4.35476 14.9351 1.99976 11.9971 1.99976C9.06008 1.99976 6.70508 4.35476 6.70508 7.29176C6.70508 10.2288 9.06008 12.5838 11.9971 12.5838Z"
+                                            fill="currentColor"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div>
+                                <h1>21K</h1>
+                                <p class="mb-0">Order Served</p>
+                            </div>
+                            <div>
+                                <div class="badge bg-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" viewBox="0 0 20 20"
+                                        fill="currentColor">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                    <span>3.48%</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-
+            <div class="col-xl-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-itmes-center">
+                            <div>
+                                <div class="p-3 rounded bg-soft-primary">
+                                    <svg class="icon-30" width="30" viewBox="0 0 24 24" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M11.997 15.1746C7.684 15.1746 4 15.8546 4 18.5746C4 21.2956 7.661 21.9996 11.997 21.9996C16.31 21.9996 19.994 21.3206 19.994 18.5996C19.994 15.8786 16.334 15.1746 11.997 15.1746Z"
+                                            fill="currentColor"></path>
+                                        <path opacity="0.4"
+                                            d="M11.9971 12.5838C14.9351 12.5838 17.2891 10.2288 17.2891 7.29176C17.2891 4.35476 14.9351 1.99976 11.9971 1.99976C9.06008 1.99976 6.70508 4.35476 6.70508 7.29176C6.70508 10.2288 9.06008 12.5838 11.9971 12.5838Z"
+                                            fill="currentColor"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div>
+                                <h1>21K</h1>
+                                <p class="mb-0">Order Served</p>
+                            </div>
+                            <div>
+                                <div class="badge bg-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" viewBox="0 0 20 20"
+                                        fill="currentColor">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                    <span>3.48%</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-itmes-center">
+                            <div>
+                                <div class="p-3 rounded bg-soft-primary">
+                                    <svg class="icon-30" width="30" viewBox="0 0 24 24" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M11.997 15.1746C7.684 15.1746 4 15.8546 4 18.5746C4 21.2956 7.661 21.9996 11.997 21.9996C16.31 21.9996 19.994 21.3206 19.994 18.5996C19.994 15.8786 16.334 15.1746 11.997 15.1746Z"
+                                            fill="currentColor"></path>
+                                        <path opacity="0.4"
+                                            d="M11.9971 12.5838C14.9351 12.5838 17.2891 10.2288 17.2891 7.29176C17.2891 4.35476 14.9351 1.99976 11.9971 1.99976C9.06008 1.99976 6.70508 4.35476 6.70508 7.29176C6.70508 10.2288 9.06008 12.5838 11.9971 12.5838Z"
+                                            fill="currentColor"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div>
+                                <h1>21K</h1>
+                                <p class="mb-0">Order Served</p>
+                            </div>
+                            <div>
+                                <div class="badge bg-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" viewBox="0 0 20 20"
+                                        fill="currentColor">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                    <span>3.48%</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-itmes-center">
+                            <div>
+                                <div class="p-3 rounded bg-soft-primary">
+                                    <svg class="icon-30" width="30" viewBox="0 0 24 24" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M11.997 15.1746C7.684 15.1746 4 15.8546 4 18.5746C4 21.2956 7.661 21.9996 11.997 21.9996C16.31 21.9996 19.994 21.3206 19.994 18.5996C19.994 15.8786 16.334 15.1746 11.997 15.1746Z"
+                                            fill="currentColor"></path>
+                                        <path opacity="0.4"
+                                            d="M11.9971 12.5838C14.9351 12.5838 17.2891 10.2288 17.2891 7.29176C17.2891 4.35476 14.9351 1.99976 11.9971 1.99976C9.06008 1.99976 6.70508 4.35476 6.70508 7.29176C6.70508 10.2288 9.06008 12.5838 11.9971 12.5838Z"
+                                            fill="currentColor"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div>
+                                <h1>21K</h1>
+                                <p class="mb-0">Order Served</p>
+                            </div>
+                            <div>
+                                <div class="badge bg-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" viewBox="0 0 20 20"
+                                        fill="currentColor">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                    <span>3.48%</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-itmes-center">
+                            <div>
+                                <div class="p-3 rounded bg-soft-primary">
+                                    <svg class="icon-30" width="30" viewBox="0 0 24 24" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M11.997 15.1746C7.684 15.1746 4 15.8546 4 18.5746C4 21.2956 7.661 21.9996 11.997 21.9996C16.31 21.9996 19.994 21.3206 19.994 18.5996C19.994 15.8786 16.334 15.1746 11.997 15.1746Z"
+                                            fill="currentColor"></path>
+                                        <path opacity="0.4"
+                                            d="M11.9971 12.5838C14.9351 12.5838 17.2891 10.2288 17.2891 7.29176C17.2891 4.35476 14.9351 1.99976 11.9971 1.99976C9.06008 1.99976 6.70508 4.35476 6.70508 7.29176C6.70508 10.2288 9.06008 12.5838 11.9971 12.5838Z"
+                                            fill="currentColor"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div>
+                                <h1>21K</h1>
+                                <p class="mb-0">Order Served</p>
+                            </div>
+                            <div>
+                                <div class="badge bg-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" viewBox="0 0 20 20"
+                                        fill="currentColor">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                    <span>3.48%</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-itmes-center">
+                            <div>
+                                <div class="p-3 rounded bg-soft-primary">
+                                    <svg class="icon-30" width="30" viewBox="0 0 24 24" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M11.997 15.1746C7.684 15.1746 4 15.8546 4 18.5746C4 21.2956 7.661 21.9996 11.997 21.9996C16.31 21.9996 19.994 21.3206 19.994 18.5996C19.994 15.8786 16.334 15.1746 11.997 15.1746Z"
+                                            fill="currentColor"></path>
+                                        <path opacity="0.4"
+                                            d="M11.9971 12.5838C14.9351 12.5838 17.2891 10.2288 17.2891 7.29176C17.2891 4.35476 14.9351 1.99976 11.9971 1.99976C9.06008 1.99976 6.70508 4.35476 6.70508 7.29176C6.70508 10.2288 9.06008 12.5838 11.9971 12.5838Z"
+                                            fill="currentColor"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div>
+                                <h1>21K</h1>
+                                <p class="mb-0">Order Served</p>
+                            </div>
+                            <div>
+                                <div class="badge bg-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" viewBox="0 0 20 20"
+                                        fill="currentColor">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                    <span>3.48%</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     @include('components.dashfooter')
