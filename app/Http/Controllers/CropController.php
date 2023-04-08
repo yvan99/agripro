46987c17cbb52838d900c2ec934a9c352e9c94cb
yvan99 <ishimweyvan90@gmail.com>
2023-04-08 19:42:12 +0200

    feature

--- app/Http/Controllers/CropController.php
+++ app/Http/Controllers/CropController.php
@@ -6,6 +6,7 @@ use App\Models\Crop;
 use App\Models\Season;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CropController extends Controller
 {
@@ -13,14 +14,38 @@ class CropController extends Controller
     {
         $crops = Crop::with('farmer', 'season')->where('farmer_id', auth()->user()->id)->get();
         $seasons = Season::all();
-        return view('crops.index', compact('crops','seasons'));
+        return view('crops.index', compact('crops', 'seasons'));
     }
 
     public function cropAdmin()
     {
         $crops = Crop::with('farmer', 'season')->get();
         $seasons = Season::all();
-        return view('crops.admin', compact('crops','seasons'));
+
+        $data = DB::table('crops')
+            ->join('seasons', 'crops.season_id', '=', 'seasons.id')
+            ->select('seasons.name', 'crops.seed_type', DB::raw('SUM(crops.yield) as total_yield'))
+            ->groupBy('seasons.name', 'crops.seed_type')
+            ->get();
+
+        $chartData = [];
+        $labels = [];
+
+        foreach ($data as $row) {
+            $labels[] = $row->name . ' - ' . $row->seed_type;
+            $chartData[] = $row->total_yield;
+        }
+
+        $cropPieData = DB::table('crops')
+        ->join('seasons', 'crops.season_id', '=', 'seasons.id')
+        ->select('seasons.name as season', 'crops.yield')
+        ->get()
+        ->groupBy('season')
+        ->map(function ($groupedData) {
+            return $groupedData->sum('yield');
+        });
+
+        return view('crops.admin', compact('crops', 'seasons', 'chartData', 'labels','cropPieData'));
     }
 
     public function store(Request $request)
--- resources/views/crops/admin.blade.php
+++ resources/views/crops/admin.blade.php
@@ -8,6 +8,35 @@
     </div>
     <div class="content-inner container-fluid pb-0" id="page_layout">
         <div class="row">
+
+            <div class="col-sm-8">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between">
+                        <div class="header-title">
+                            <h4 class="card-title">Crop Yield Harvest Analysis</h4>
+
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="crop-season-yield-chart"></canvas>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between">
+                        <div class="header-title">
+                            <h4 class="card-title">Seasonan Analysis</h4>
+
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="pieChart"></canvas>
+                    </div>
+                </div>
+            </div>
+
             <div class="col-sm-12">
                 <div class="card">
                     <div class="card-header d-flex justify-content-between">
@@ -46,7 +75,7 @@
                                             <td>{{ $crop->fertilizer_amount }}</td>
                                             <td>{{ $crop->pesticide_type }}</td>
                                             <td>{{ $crop->yield }}</td>
-                                           
+
                                         </tr>
                                     @endforeach
                                 </tbody>
@@ -60,3 +89,61 @@
     @include('components.dashfooter')
 </main>
 @include('components.dashjs')
+<script>
+    var ctx = document.getElementById('crop-season-yield-chart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: {!! json_encode($labels) !!},
+            datasets: [{
+                label: 'Crop Yield',
+                data: {!! json_encode($chartData) !!},
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+
+    // PIE DATA
+
+    // Get data from controller
+    var cropPieData = {!! json_encode($cropPieData) !!};
+
+    // Extract season names and corresponding crop yields
+    var seasonNames = Object.keys(cropPieData);
+    var cropYields = Object.values(cropPieData);
+
+    // Generate random colors for pie chart slices
+    var colors = seasonNames.map(function(seasonName) {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    });
+
+    // Create pie chart
+    var pieChart = new Chart(document.getElementById("pieChart"), {
+        type: 'pie',
+        data: {
+            labels: seasonNames,
+            datasets: [{
+                backgroundColor: colors,
+                data: cropYields
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Crop yields by season'
+            }
+        }
+    });
+</script>
