79f2fd9ba4b71e394667bf53f4bb23e4fafac967
yvan99 <ishimweyvan90@gmail.com>
2023-04-08 12:08:52 +0200

    feature

---
+++ resources/views/crops/admin.blade.php
@@ -0,0 +1,62 @@
+@include('components.dashcss')
+@include('farmer.components.aside')
+<main class="main-content">
+    <div class="position-relative ">
+        <!--Nav Start-->
+        @include('components.dasheader')
+        <!--Nav End-->
+    </div>
+    <div class="content-inner container-fluid pb-0" id="page_layout">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between">
+                        <div class="header-title">
+                            <h4 class="card-title">Crops List</h4>
+
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive border rounded">
+                            <table id="datatable" class="table " data-toggle="data-table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Crop Type</th>
+                                        {{-- <th>Farmer</th> --}}
+                                        <th>Season</th>
+                                        <th>Area</th>
+                                        <th>Planting Date</th>
+                                        <th>Seed Type</th>
+                                        <th>Fertilizer Amount</th>
+                                        <th>Pesticide Type</th>
+                                        <th>Yield</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($crops as $crop)
+                                        <tr>
+                                            <th scope="row">{{ $crop->id }}</th>
+                                            <td>{{ $crop->crop_type }}</td>
+                                            {{-- <td>{{ $crop->farmer->name }}</td> --}}
+                                            <td> <span class="badge bg-success">{{ $crop->season->name }}</span> </td>
+                                            <td>{{ $crop->area }}</td>
+                                            <td>{{ $crop->planting_date }}</td>
+                                            <td>{{ $crop->seed_type }}</td>
+                                            <td>{{ $crop->fertilizer_amount }}</td>
+                                            <td>{{ $crop->pesticide_type }}</td>
+                                            <td>{{ $crop->yield }}</td>
+                                           
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @include('components.dashfooter')
+</main>
+@include('components.dashjs')
